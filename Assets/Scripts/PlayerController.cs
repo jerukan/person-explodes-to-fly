58587ddb893ba8b0caba4afe88ea246f0c5bfe4e
Jerry <jerry21yan@gmail.com>
2019-02-28 12:28:40 -0800

    import lots of standard assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+[RequireComponent(typeof (RigidbodyFirstPersonController))]
+[RequireComponent(typeof (Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    public RigidbodyFirstPersonController charController;
+
+    // Use this for initialization
+    void Start() {
+        charController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            Vector3 vector = charController.cam.transform.forward;
+        }
+    }
+}
